491309b67c6cdb3a3b9d8e9a95f1046ba3d3ce31
thomas1908 <thomas.pottier@supinfo.com>
2024-04-04 13:22:07 +0200

    update

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 class Board:
     def __init__(self):
-        self.board = [['o' for _ in range(11)] for _ in range(11)]
+        self.board = [[None, None, None, None, 4, None, 6, None, None, None, None],
+                      [None, None, None, 3, None, 5, None, 7, None, None, None],
+                      [None, None, 2, None, 4, None, 6, None, 8, None, None],
+                      [None, 1, None, 3, None, 5, None, 7, None, 9, None],
+                      [None, None, 2, None, 4, None, 6, None, 8, None, None],
+                      [None, 1, None, 3, None, 5, None, 7, None, 9, None],
+                      [0, None, 2, None, 4, None, 6, None, 8, None, 10],
+                      [None, 1, None, 3, None, 5, None, 7, None, 9, None],
+                      [0, None, 2, None, 4, None, 6, None, 8, None, 10],
+                      [None, 1, None, 3, None, 5, None, 7, None, 9, None],
+                      [0, None, 2, None, 4, None, 6, None, 8, None, 10],
+                      [None, 1, None, 3, None, 5, None, 7, None, 9, None],
+                      [0, None, 2, None, 4, None, 6, None, 8, None, 10],
+                      [None, 1, None, 3, None, 5, None, 7, None, 9, None],
+                      [None, None, 2, None, 4, None, 6, None, 8, None, None],
+                      [None, 1, None, 3, None, 5, None, 7, None, 9, None],
+                      [None, None, 2, None, 4, None, 6, None, 8, None, None],
+                      [None, None, None, 3, None, 5, None, 7, None, None, None],
+                      [None, None, None, None, 4, None, 6, None, None, None, None]]
 
     def __str__(self):
-        return '\n'.join(' '.join(row) for row in self.board)
-
+        board_str = ''
+        for row in self.board:
+            board_str += ' '.join([str(cell) if cell is not None else ' ' for cell in row]) + '\n'
+        return board_str
 
 if __name__ == '__main__':
     board = Board()
