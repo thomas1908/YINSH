27ba8fe626078abd9e81d4faf2575e4c9954793f
thomas1908 <thomas.pottier@supinfo.com>
2024-04-04 15:51:10 +0200

    update

--- main.py
+++ main.py
@@ -26,6 +26,14 @@ class Board:
             board_str += ' '.join([str(cell) if cell is not None else ' ' for cell in row]) + '\n'
         return board_str
 
+    def getCase(self, x, y):
+        return self.board[y][x]
+
+    def setCase(self, x, y, value):
+        self.board[y][x] = value
+
 if __name__ == '__main__':
     board = Board()
     print(board)
+    board.setCase(0, 0, 1)
+    print(board.getCase(0, 0))
