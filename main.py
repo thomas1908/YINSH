2a61bfbd5e712add73c8dd69564b44c7c93750fd
thomas1908 <thomas.pottier@supinfo.com>
2024-05-13 09:07:05 +0200

    update

--- main.py
+++ main.py
@@ -1,29 +1,43 @@
+class Case:
+    def __init__(self, x, y, value):
+        self.x = x
+        self.y = y
+        self.value = value
+
+    def set__Value(self, value):
+        self.value = value
+
+    def get__Value(self):
+        return self.value
+    def __str__(self):
+        return f'Case({self.x}, {self.y}, {self.value})'
+
 class Board:
     def __init__(self):
-        self.board = [[None, None, None, None, 4, None, 6, None, None, None, None],
-                      [None, None, None, 3, None, 5, None, 7, None, None, None],
-                      [None, None, 2, None, 4, None, 6, None, 8, None, None],
-                      [None, 1, None, 3, None, 5, None, 7, None, 9, None],
-                      [None, None, 2, None, 4, None, 6, None, 8, None, None],
-                      [None, 1, None, 3, None, 5, None, 7, None, 9, None],
-                      [0, None, 2, None, 4, None, 6, None, 8, None, 10],
-                      [None, 1, None, 3, None, 5, None, 7, None, 9, None],
-                      [0, None, 2, None, 4, None, 6, None, 8, None, 10],
-                      [None, 1, None, 3, None, 5, None, 7, None, 9, None],
-                      [0, None, 2, None, 4, None, 6, None, 8, None, 10],
-                      [None, 1, None, 3, None, 5, None, 7, None, 9, None],
-                      [0, None, 2, None, 4, None, 6, None, 8, None, 10],
-                      [None, 1, None, 3, None, 5, None, 7, None, 9, None],
-                      [None, None, 2, None, 4, None, 6, None, 8, None, None],
-                      [None, 1, None, 3, None, 5, None, 7, None, 9, None],
-                      [None, None, 2, None, 4, None, 6, None, 8, None, None],
-                      [None, None, None, 3, None, 5, None, 7, None, None, None],
-                      [None, None, None, None, 4, None, 6, None, None, None, None]]
+        self.board = [[None, None, None, None, Case, None, Case, None, None, None, None],
+                      [None, None, None, Case, None, Case, None, Case, None, None, None],
+                      [None, None, Case, None, Case, None, Case, None, Case, None, None],
+                      [None, Case, None, Case, None, Case, None, Case, None, Case, None],
+                      [None, None, Case, None, Case, None, Case, None, Case, None, None],
+                      [None, Case, None, Case, None, Case, None, Case, None, Case, None],
+                      [Case, None, Case, None, Case, None, Case, None, Case, None, Case],
+                      [None, Case, None, Case, None, Case, None, Case, None, Case, None],
+                      [Case, None, Case, None, Case, None, Case, None, Case, None, Case],
+                      [None, Case, None, Case, None, Case, None, Case, None, Case, None],
+                      [Case, None, Case, None, Case, None, Case, None, Case, None, Case],
+                      [None, Case, None, Case, None, Case, None, Case, None, Case, None],
+                      [Case, None, Case, None, Case, None, Case, None, Case, None, Case],
+                      [None, Case, None, Case, None, Case, None, Case, None, Case, None],
+                      [None, None, Case, None, Case, None, Case, None, Case, None, None],
+                      [None, Case, None, Case, None, Case, None, Case, None, Case, None],
+                      [None, None, Case, None, Case, None, Case, None, Case, None, None],
+                      [None, None, None, Case, None, Case, None, Case, None, None, None],
+                      [None, None, None, None, Case, None, Case, None, None, None, None]]
 
     def __str__(self):
         board_str = ''
         for row in self.board:
-            board_str += ' '.join([str(cell) if cell is not None else ' ' for cell in row]) + '\n'
+            board_str += ' '.join([str('◉') if cell is not None else ' ' for cell in row]) + '\n'
         return board_str
 
     def getCase(self, x, y):
@@ -35,5 +49,6 @@ class Board:
 if __name__ == '__main__':
     board = Board()
     print(board)
-    board.setCase(0, 0, 1)
-    print(board.getCase(0, 0))
+    board.getCase(4, 0).set__Value(2)
+    print(board.getCase(4, 0).get__Value())
+    print(board)
