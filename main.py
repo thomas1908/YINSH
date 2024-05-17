b5b090e0ca5c4043b00e95366a49aa14f781eeee
thomas1908 <thomas.pottier@supinfo.com>
2024-05-17 15:03:34 +0200

    update main

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 import wx
 
 class Case:
-    def __init__(self, x, y, value=1):
+    def __init__(self, x, y, value=1, ring = False):
         self.x = x
         self.y = y
         self.value = value  # 1 for empty, 2 ring player 1, 3 ring player 2, 4 pawn player 1, 5 pawn player 2
+        self.ring = ring
 
     def set__Value(self, value):
         self.value = value
 
     def get__Value(self):
         return self.value
+    def get__ring(self):
+        return self.ring
+
+    def set__ring(self, ring):
+        self.ring = ring
 
 class BoardPanel(wx.Panel):
     def __init__(self, parent):
@@ -41,6 +47,7 @@ class BoardPanel(wx.Panel):
         self.current_player = 1
         self.player_pawns = {1: 0, 2: 0}
         self.max_pawns = 5
+        self.selected_pawn = None
 
     def on_paint(self, event):
         dc = wx.PaintDC(self)
@@ -60,23 +67,126 @@ class BoardPanel(wx.Panel):
                         pen = gc.CreatePen(wx.Pen(wx.Colour(128, 128, 128), width=3))
                         gc.SetPen(pen)
                         gc.DrawEllipse(case.x * 40 + 5, case.y * 40 + 5, 30, 30)
-                    elif case.get__Value() == 4:
+                    elif case.get__ring() == True and case.get__Value() == 4:
                         dc.SetBrush(wx.Brush("black"))
-                        dc.DrawCircle(case.x * 40 + 20, case.y * 40 + 20, 10)
-                    elif case.get__Value() == 5:
+                        dc.DrawCircle(case.x * 40 + 20, case.y * 40 + 20, 25)
+                        pen = gc.CreatePen(wx.Pen(wx.WHITE, width=3))
+                        gc.SetPen(pen)
+                        gc.DrawEllipse(case.x * 40 + 5, case.y * 40 + 5, 30, 30)
+                    elif case.get__ring() == True and case.get__Value() == 5:
                         dc.SetBrush(wx.Brush("grey"))
-                        dc.DrawCircle(case.x * 40 + 20, case.y * 40 + 20, 10)
+                        dc.DrawCircle(case.x * 40 + 20, case.y * 40 + 20, 25)
+                        pen = gc.CreatePen(wx.Pen(wx.Colour(255, 255, 255), width=3))
+                        gc.SetPen(pen)
+                        gc.DrawEllipse(case.x * 40 + 5, case.y * 40 + 5, 30, 30)
+                    elif case.get__Value() == 4 and case.get__ring() == False:
+                        pen = gc.CreatePen(wx.Pen(wx.BLACK, width=3))
+                        gc.SetPen(pen)
+                        gc.DrawEllipse(case.x * 40 + 5, case.y * 40 + 5, 30, 30)
+                        dc.SetBrush(wx.Brush("black"))
+                        dc.DrawCircle(case.x * 40 + 20, case.y * 40 + 20, 15)
+                    elif case.get__Value() == 5 and case.get__ring() == False:
+                        dc.SetBrush(wx.Brush("grey"))
+                        dc.DrawCircle(case.x * 40 + 20, case.y * 40 + 20, 15)
+
     def on_left_down(self, event):
         pos = event.GetPosition()
         x = pos.x // 40
         y = pos.y // 40
         if 0 <= x < 11 and 0 <= y < 19:
-            if self.player_pawns[self.current_player] < self.max_pawns:
-                if self.board[y][x].get__Value() == 1:
-                    self.board[y][x].set__Value(self.current_player+1)
-                    self.player_pawns[self.current_player] += 1
-                    self.Refresh()
-                    self.switch_player()
+            if self.selected_pawn is None:  # If no pawn is selected
+                if self.player_pawns[self.current_player] < self.max_pawns:
+                    if self.board[y][x].get__Value() == 1:
+                        self.board[y][x].set__Value(self.current_player + 1)
+                        self.board[y][x].set__ring(True)
+                        self.player_pawns[self.current_player] += 1
+                        self.Refresh()
+                        self.switch_player()
+                else:
+                    if self.board[y][x].get__Value() == self.current_player + 1 and self.board[y][x].ring == True:
+                        self.board[y][x].set__Value(self.current_player + 3)
+                        self.Refresh()
+                        self.selected_pawn = (x, y)  # Select the pawn
+            else:  # If a pawn is selected
+                if self.board[y][x].get__Value() == 1:  # If the destination is empty
+                    self.move_ring(*self.selected_pawn, x, y)  # Move the pawn
+                    self.selected_pawn = None
+                    if self.check_five_in_a_row(x, y):
+                        self.remove_markers(x, y)
+
+
+    def move_ring(self, start_x, start_y, end_x, end_y):
+        start_case = self.board[start_y][start_x]
+        end_case = self.board[end_y][end_x]
+        if self.is_valid_move(start_x, start_y, end_x, end_y):
+            end_case.set__Value(start_case.get__Value()-2)
+            start_case.set__Value(4 if self.current_player == 1 else 5)
+            start_case.set__ring(False)
+            end_case.set__ring(True)
+            self.convert_markers(start_x, start_y, end_x, end_y)
+            self.Refresh()
+            self.switch_player()
+
+    def convert_markers(self, start_x, start_y, end_x, end_y):
+        direction = (end_x - start_x, end_y - start_y)
+        steps = max(abs(direction[0]), abs(direction[1]))
+        dx = direction[0] // steps
+        dy = direction[1] // steps
+
+        x, y = start_x + dx, start_y + dy
+        while (x, y) != (end_x, end_y):
+            case = self.board[y][x]
+            if case.get__Value() in (4, 5):
+                new_value = 2 if self.current_player == 1 else 3
+                case.set__Value(new_value)
+                case.set__ring(True)
+            x += dx
+            y += dy
+        print(self.board[y][x].get__Value(), self.board[y][x].get__ring())
+
+    def is_valid_move(self, start_x, start_y, end_x, end_y):
+        direction = (end_x - start_x, end_y - start_y)
+        steps = max(abs(direction[0]), abs(direction[1]))
+        dx = direction[0] // steps
+        dy = direction[1] // steps
+        x, y = start_x + dx, start_y + dy
+
+        while (x, y) != (end_x, end_y):
+            case = self.board[y][x]
+            if case.get__Value() in (2, 3) or (case.get__Value() in (4, 5) and (x, y) != (end_x, end_y)):
+                return False
+            x += dx
+            y += dy
+        return True
+
+    def remove_markers(self, x, y):
+        directions = [(1, 0), (0, 1), (1, 1), (-1, 1)]
+        for dx, dy in directions:
+            for i in range(5):
+                nx, ny = x + i * dx, y + i * dy
+                if 0 <= nx < 11 and 0 <= ny < 19:
+                    case = self.board[ny][nx]
+                    if case.get__Value() in (4, 5):
+                        case.set__Value(1)
+        self.Refresh()
+
+    def check_five_in_a_row(self, x, y):
+        directions = [(1, 0), (0, 1), (1, 1), (-1, 1)]
+        for dx, dy in directions:
+            count = 0
+            for i in range(5):
+                nx, ny = x + i * dx, y + i * dy
+                if 0 <= nx < 11 and 0 <= ny < 19 and self.board[ny][nx] != 0:
+                    value = self.board[ny][nx].get__Value()
+                    if value in (4, 5):
+                        count += 1
+                    else:
+                        break
+                else:
+                    break
+            if count == 5:
+                return True
+        return False
 
     def switch_player(self):
         self.current_player = 3 - self.current_player
